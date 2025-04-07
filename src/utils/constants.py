613d822157ff463d6c93b8b07aeb0e9871ed70df
Junha Lee <junhalee@stanford.edu>
2025-04-06 23:59:47 -0700

    infrastructure complete

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -7,7 +7,11 @@ DATA_DIR = HOME_DIR + "data/"
 CHEMBL_DATA_DIR = DATA_DIR + "chembl_35/chembl_35_sqlite/"
 
 MODELS_DIR = HOME_DIR + "models/"
-LLAMA_3P3_70B_MODEL_DIR = MODELS_DIR + "models--meta-llama--Llama-3.3-70B-Instruct"
+LLAMA_3P3_70B_MODEL_DIR = MODELS_DIR + "models--meta-llama--Llama-3.3-70B-Instruct/snapshots/6f6073b423013f6a7d4d9f39144961bfbfbc386b/"
 
 # File paths
 CHEMBL_DATA_FILE = CHEMBL_DATA_DIR + "chembl_clip_abstracts.h5"
+
+
+# Model names
+LLAMA_3P3_70B_MODEL_NAME = "meta-llama/Llama-3.3-70B-Instruct"
